e4c19c8968a30f68e767a7a0e5c5a69301f8fce4
Santiago Blanco <sblancov84@gmail.com>
2018-05-04 00:16:06 +0200

    chore: Add configuration files for distribution

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyaclg',
+    version='0.1.1',
+    description='Auomatic Changelog Git',
+    author='Santiago Blanco Ventas',
+    license='MIT',
+    project_urls={
+        'Source': 'https://github.com/sblancov/pyaclg'
+    },
+    packages=find_packages(),
+    install_requires=['gitpython==2.1.9'],
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': ['pyaclg = pyaclg.main:main']
+    }
+)
