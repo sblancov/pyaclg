2919b44739bb93a0b27a65b8b76b2eed72ec39fb
Santiago Blanco <sblancov84@gmail.com>
2018-05-04 23:19:52 +0200

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyaclg',
-    version='0.1.1',
+    version='0.2.0',
     description='Auomatic Changelog Git',
     author='Santiago Blanco Ventas',
     license='MIT',
